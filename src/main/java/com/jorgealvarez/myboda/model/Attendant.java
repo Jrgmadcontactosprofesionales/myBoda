0a314234afc173b2ea7160d77b33f607e43fcb15
jrgmad <72515345+Jrgmadcontactosprofesionales@users.noreply.github.com>
2022-06-11 03:51:09 +0200

    - dob cambiado a string. No da error pero hay que arreglar formato.   - Scripts de list-attendants.html sacados fuera.   - Elementos de tabla traducidos.

--- src/main/java/com/jorgealvarez/myboda/model/Attendant.java
+++ src/main/java/com/jorgealvarez/myboda/model/Attendant.java
@@ -3,36 +3,33 @@ package com.jorgealvarez.myboda.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.util.Date;
 
 @Entity
-@Table(name="attendant")
+@Table(name = "attendant")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Attendant {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
-	private String name;
 
-	private String surname;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-//	TODO: solucionar el parse del jason que llega en String a LocalDate
-	@DateTimeFormat(pattern = "dd-MM-yyyy")
-	private LocalDate dob;
+    private String name;
 
-	private String ph;
+    private String surname;
 
-	private Status attendingStatus;
+    /*TODO: solucionar el parse del jason que llega en String a LocalDate
+        @DateFormat(pattern = "dd-MM-yyyy")*/
+    private String dob;
 
-	private String specialRequirement;
+    private String ph;
 
-	private Availability specialRequirementStatus;
+    private Status attendingStatus;
+
+    private String specialRequirement;
+
+    private Availability specialRequirementStatus;
 }
